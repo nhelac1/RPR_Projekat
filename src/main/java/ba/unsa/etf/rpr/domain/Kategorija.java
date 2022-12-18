885af7663cffc1e320700e51681d4f5352b0f25b
Nedzla Helac <nhelac1@etf.unsa.ba>
2022-12-18 14:57:01 +0100

    Dodani atributi za klasu Kategorija

--- src/main/java/ba/unsa/etf/rpr/domain/Kategorija.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Kategorija.java
@@ -1,4 +1,45 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Kategorija {
+    private int id;
+    private String ime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    @Override
+    public String toString() {
+        return "Kategorija{" +
+                "id=" + id +
+                ", ime='" + ime + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Kategorija kategorija = (Kategorija) o;
+        return id == kategorija.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, ime);
+    }
 }
