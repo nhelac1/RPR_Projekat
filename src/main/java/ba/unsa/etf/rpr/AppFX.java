6e455055e2d561da31da502782a004bb5162a120
Nedzla Helac <nhelac1@etf.unsa.ba>
2022-12-18 17:01:55 +0100

    Dodan dependency za javafx, napravljena JavaFX App i dodani paketi controllers i exception. <greska> Could not find or load main class javafx.fxml

---
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppFX extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/ba/unsa/etf/rpr/exceptions/CosmeticException.java
@@ -0,0 +1,4 @@
+package ba.unsa.etf.rpr.exceptions;
+
+public class CosmeticException {
+}
